8667dcaa9ab00ad3854810f0215aaf3568c0a347
Kirill <kirill2001krav@mail.ru>
2024-05-08 23:18:54 +0300

    Added reflection and fixed specular

--- Material.hpp
+++ Material.hpp
@@ -6,16 +6,18 @@
 class Material
 {
 public:
-    Material(const Vec &ac, const Vec &dc, const Vec &sc, const float s) : ambientColor(ac), diffuseColor(dc), specularColor(sc), shininess(s) {}
-    Material() : ambientColor(), diffuseColor(), specularColor(), shininess() {}
+    Material(const Vec &ac, const Vec &dc, const Vec &sc, const float s, const float r) : ambientColor(ac), diffuseColor(dc), specularColor(sc), shininess(s), reflection(r) {}
+    Material() : ambientColor(), diffuseColor(), specularColor(), shininess(), reflection() {}
     Vec getAmbientColor() { return ambientColor; }
     Vec getDiffuseColor() { return diffuseColor; }
     Vec getSpecularColor() { return specularColor; }
     float getShininess() { return shininess; }
+    float getReflection() { return reflection; }
 
 private:
     Vec ambientColor;
     Vec diffuseColor;
     Vec specularColor;
     float shininess;
+    float reflection;
 };
--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@
 #include "sphere.hpp"
 #include "light.hpp"
 
-
 bool scene_intersect(const Vec &orig, const Vec &dir, const std::vector<Sphere> &spheres, Material &material, Vec &point, Vec &N)
 {
     float sphereDist = std::numeric_limits<float>::max();
@@ -30,23 +29,38 @@ bool scene_intersect(const Vec &orig, const Vec &dir, const std::vector<Sphere> 
     return sphereDist < 1000;
 }
 
-Vec cast_ray(const Vec &orig, const Vec &dir, const std::vector<Sphere> &spheres, const std::vector<Light> &lights)
+Vec reflect(const Vec &I, const Vec &N)
+{
+    return N * 2.f * Vec::dot(I, N) - I;
+}
+
+Vec cast_ray(const Vec &orig, const Vec &dir, const std::vector<Sphere> &spheres, const std::vector<Light> &lights, int depth = 0)
 {
     Material material;
     Vec point, N;
 
-    if (scene_intersect(orig, dir, spheres, material, point, N))
+    if (depth < 10 && scene_intersect(orig, dir, spheres, material, point, N))
     {
         Vec res(0.0f, 0.0f, 0.0f);
-        for(int i = 0; i < lights.size(); i++){
+        Vec reflect_dir = reflect(dir * -1.0, N).normalize();
+        Vec reflect_orig = Vec::dot(reflect_dir, N) < 0 ? point - N * 1e-3 : point + N * 1e-3;
+        Vec reflect_color = cast_ray(reflect_orig, reflect_dir, spheres, lights, depth + 1);
+        for (int i = 0; i < lights.size(); i++)
+        {
             Vec lightDir = (lights[i].getPosition() - point).normalize();
+            float lightDist = (lights[i].getPosition() - point).distance();
+            Vec shadowOrig = Vec::dot(lightDir, N) < 0 ? point - N * 1e-3 : point + N * 1e-3;
+            Vec shadowPoint, shadowN;
+            Material tmpmaterial;
+            if (scene_intersect(shadowOrig, lightDir, spheres, tmpmaterial, shadowPoint, shadowN))
+                continue;
             Vec ambient_light_intensity = material.getAmbientColor() * lights[i].getAmbientColor();
             Vec diffuse_light_intensity = material.getDiffuseColor() * std::max(0.0f, Vec::dot(lightDir, N)) * lights[i].getDiffuseColor();
-            Vec specular_light_intensity = material.getSpecularColor() * std::pow(std::max(0.0f, Vec::dot(lightDir, N)), material.getShininess()) * lights[i].getSpecularColor();
+            Vec specular_light_intensity = material.getSpecularColor() * std::pow(std::max(0.0f, Vec::dot(reflect(lightDir * -1.0, N), dir)), material.getShininess()) * lights[i].getSpecularColor();
             res = res + (ambient_light_intensity + diffuse_light_intensity + specular_light_intensity);
         }
-        
-        return res;
+
+        return res + reflect_color * material.getReflection();
     }
     else
     {
@@ -90,34 +104,38 @@ int main()
     Material gold(Vec(0.24725f, 0.1995f, 0.0745f),
                   Vec(0.75164f, 0.60648f, 0.22648f),
                   Vec(0.628281f, 0.555802f, 0.366065f),
-                  0.4f * 128);
+                  0.4f * 128, 0.1);
     Material ruby(Vec(0.1745f, 0.01175f, 0.01175f),
                   Vec(0.61424f, 0.04136f, 0.04136f),
                   Vec(0.727811f, 0.626959f, 0.626959f),
-                  0.6f * 128);
+                  0.6f * 128, 0.1);
+    Material mirror(Vec(0.1, 0.1, 0.1),
+                    Vec(0.1, 0.1, 0.1),
+                    Vec(1.0, 1.0, 1.0),
+                    1428.0, 0.8);
     std::vector<Sphere> spheres;
     // spheres.push_back(Sphere(Vec(-3.0f, 0.0f, -16.0f), 2.0f, gold));
-    // spheres.push_back(Sphere(Vec(-1.0f, -1.5f, -12.0f), 2.0f, ruby));
+    // spheres.push_back(Sphere(Vec(-1.0f, -1.5f, -12.0f), 2.0f, mirror));
     // spheres.push_back(Sphere(Vec(1.5f, -0.5f, -18.0f), 3.0f, ruby));
-    // spheres.push_back(Sphere(Vec(7.0f, 5.0f, -18.0f), 4.0f, gold));
+    // spheres.push_back(Sphere(Vec(7.0f, 5.0f, -18.0f), 4.0f, mirror));
     spheres.push_back(Sphere(Vec(-10, 5, -18), 4, gold));
-    spheres.push_back(Sphere(Vec(0, 2, -40), 14, ruby));
-    spheres.push_back(Sphere(Vec(16, 5, -41), 9, gold));
+    spheres.push_back(Sphere(Vec(0, 2, -40), 14, mirror));
+    spheres.push_back(Sphere(Vec(16, 5, -41), 9, mirror));
     spheres.push_back(Sphere(Vec(4, -5, -10), 4, ruby));
     std::vector<Light> lights;
-    lights.push_back(Light( Vec(-20.0f, 20.0f, 20.0f),
-                            Vec(0.2f, 0.2f, 0.2f),
-                            Vec(0.9f, 0.9f, 0.9f),
-                            Vec(1.0f, 1.0f, 1.0f)));
-    lights.push_back(Light( Vec(30.0f, 50.0f, -25.0f),
-                            Vec(0.2f, 0.2f, 0.2f),
-                            Vec(0.9f, 0.9f, 0.9f),
-                            Vec(1.0f, 1.0f, 1.0f)));
-    lights.push_back(Light( Vec(30.0f, 20.0f, 30.0f),
-                            Vec(0.2f, 0.2f, 0.2f),
-                            Vec(0.9f, 0.9f, 0.9f),
-                            Vec(1.0f, 1.0f, 1.0f)));
-    
+    lights.push_back(Light(Vec(-20.0f, 20.0f, 20.0f),
+                           Vec(0.2f, 0.2f, 0.2f),
+                           Vec(0.9f, 0.9f, 0.9f),
+                           Vec(1.0f, 1.0f, 1.0f)));
+    lights.push_back(Light(Vec(30.0f, 50.0f, -25.0f),
+                           Vec(0.2f, 0.2f, 0.2f),
+                           Vec(0.9f, 0.9f, 0.9f),
+                           Vec(1.0f, 1.0f, 1.0f)));
+    lights.push_back(Light(Vec(30.0f, 20.0f, 30.0f),
+                           Vec(0.2f, 0.2f, 0.2f),
+                           Vec(0.9f, 0.9f, 0.9f),
+                           Vec(1.0f, 1.0f, 1.0f)));
+
     render(spheres, lights);
     return 0;
 }
