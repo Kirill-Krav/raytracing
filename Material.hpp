dc93997e817a8e3da7b543b2c0bb2a2e9736d2b4
Kirill <kirill2001krav@mail.ru>
2024-05-09 09:36:05 +0300

    Added refraction

--- Material.hpp
+++ Material.hpp
@@ -6,13 +6,15 @@
 class Material
 {
 public:
-    Material(const Vec &ac, const Vec &dc, const Vec &sc, const float s, const float r) : ambientColor(ac), diffuseColor(dc), specularColor(sc), shininess(s), reflection(r) {}
-    Material() : ambientColor(), diffuseColor(), specularColor(), shininess(), reflection() {}
+    Material(const Vec &ac, const Vec &dc, const Vec &sc, const float s, const float refl, const float refr, const float ri) : ambientColor(ac), diffuseColor(dc), specularColor(sc), shininess(s), reflection(refl), refraction(refr), refractiveIndex(ri) {}
+    Material() : ambientColor(), diffuseColor(), specularColor(), shininess(), reflection(), refraction(), refractiveIndex() {}
     Vec getAmbientColor() { return ambientColor; }
     Vec getDiffuseColor() { return diffuseColor; }
     Vec getSpecularColor() { return specularColor; }
     float getShininess() { return shininess; }
     float getReflection() { return reflection; }
+    float getRefraction() { return refraction; }
+    float getRefractiveIndex() { return refractiveIndex; }
 
 private:
     Vec ambientColor;
@@ -20,4 +22,6 @@ private:
     Vec specularColor;
     float shininess;
     float reflection;
+    float refraction;
+    float refractiveIndex;
 };
--- main.cpp
+++ main.cpp
@@ -34,17 +34,33 @@ Vec reflect(const Vec &I, const Vec &N)
     return N * 2.f * Vec::dot(I, N) - I;
 }
 
+Vec refract(const Vec &I, const Vec &N, const float &refractive_index) {
+    float cosi = std::max(-1.f, std::min(1.f, Vec::dot(I,N * -1.0)));
+    float etai = 1, etat = refractive_index;
+    Vec n = N;
+    if (cosi < 0) { 
+        cosi = -cosi;
+        std::swap(etai, etat); n = N * -1.0;
+    }
+    float eta = etai / etat;
+    float k = 1 - eta*eta*(1 - cosi*cosi);
+    return k < 0 ? Vec(0,0,0) : I*eta + n*(eta * cosi - sqrtf(k));
+}
+
 Vec cast_ray(const Vec &orig, const Vec &dir, const std::vector<Sphere> &spheres, const std::vector<Light> &lights, int depth = 0)
 {
     Material material;
     Vec point, N;
 
-    if (depth < 10 && scene_intersect(orig, dir, spheres, material, point, N))
+    if (depth < 4 && scene_intersect(orig, dir, spheres, material, point, N))
     {
         Vec res(0.0f, 0.0f, 0.0f);
         Vec reflect_dir = reflect(dir * -1.0, N).normalize();
+        Vec refract_dir = refract(dir, N, material.getRefractiveIndex()).normalize();
         Vec reflect_orig = Vec::dot(reflect_dir, N) < 0 ? point - N * 1e-3 : point + N * 1e-3;
+        Vec refract_orig = Vec::dot(refract_dir, N) < 0 ? point - N*1e-3 : point + N*1e-3;
         Vec reflect_color = cast_ray(reflect_orig, reflect_dir, spheres, lights, depth + 1);
+        Vec refract_color = cast_ray(refract_orig, refract_dir, spheres, lights, depth + 1);
         for (int i = 0; i < lights.size(); i++)
         {
             Vec lightDir = (lights[i].getPosition() - point).normalize();
@@ -60,7 +76,7 @@ Vec cast_ray(const Vec &orig, const Vec &dir, const std::vector<Sphere> &spheres
             res = res + (ambient_light_intensity + diffuse_light_intensity + specular_light_intensity);
         }
 
-        return res + reflect_color * material.getReflection();
+        return res + reflect_color * material.getReflection() + refract_color * material.getRefraction();
     }
     else
     {
@@ -104,24 +120,28 @@ int main()
     Material gold(Vec(0.24725f, 0.1995f, 0.0745f),
                   Vec(0.75164f, 0.60648f, 0.22648f),
                   Vec(0.628281f, 0.555802f, 0.366065f),
-                  0.4f * 128, 0.1);
+                  0.4f * 128, 0.1, 0.0, 1.0);
     Material ruby(Vec(0.1745f, 0.01175f, 0.01175f),
                   Vec(0.61424f, 0.04136f, 0.04136f),
                   Vec(0.727811f, 0.626959f, 0.626959f),
-                  0.6f * 128, 0.1);
+                  0.6f * 128, 0.1, 0.0, 1.0);
     Material mirror(Vec(0.1, 0.1, 0.1),
                     Vec(0.1, 0.1, 0.1),
-                    Vec(1.0, 1.0, 1.0),
-                    1428.0, 0.8);
+                    Vec(10.0, 10.0, 10.0),
+                    1428.0, 0.8, 0.0, 1.0);
+    Material glass(Vec(0.0, 0.0, 0.0),
+                    Vec(0.0, 0.0, 0.0),
+                    Vec(0.5, 0.5, 0.5),
+                    125.0, 0.1, 0.8, 1.5);
     std::vector<Sphere> spheres;
     // spheres.push_back(Sphere(Vec(-3.0f, 0.0f, -16.0f), 2.0f, gold));
-    // spheres.push_back(Sphere(Vec(-1.0f, -1.5f, -12.0f), 2.0f, mirror));
+    // spheres.push_back(Sphere(Vec(-1.0f, -1.5f, -12.0f), 2.0f, glass));
     // spheres.push_back(Sphere(Vec(1.5f, -0.5f, -18.0f), 3.0f, ruby));
     // spheres.push_back(Sphere(Vec(7.0f, 5.0f, -18.0f), 4.0f, mirror));
-    spheres.push_back(Sphere(Vec(-10, 5, -18), 4, gold));
-    spheres.push_back(Sphere(Vec(0, 2, -40), 14, mirror));
-    spheres.push_back(Sphere(Vec(16, 5, -41), 9, mirror));
-    spheres.push_back(Sphere(Vec(4, -5, -10), 4, ruby));
+    spheres.push_back(Sphere(Vec(-10, 5, -18), 4, mirror));
+    spheres.push_back(Sphere(Vec(0, 2, -40), 14, ruby));
+    spheres.push_back(Sphere(Vec(16, 5, -41), 9, gold));
+    spheres.push_back(Sphere(Vec(4, -3, -10), 4, glass));
     std::vector<Light> lights;
     lights.push_back(Light(Vec(-20.0f, 20.0f, 20.0f),
                            Vec(0.2f, 0.2f, 0.2f),
